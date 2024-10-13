cffbcbc44ca46a7403248ecf939d2d4ab96e613b
Panda <mattiahag@gmail.com>
2024-10-13 12:34:03 +0200

    final

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ import { getTopProfilesByPoints, getTrendingProfiles } from './points/trendingPr
 
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
-const API_URL = process.env.NODE_ENV === "development" ? "http://localhost:8080" : "";
+const API_URL = process.env.NODE_ENV === "development" ? "http://localhost:8080" : "https://fantatipe-1-0.onrender.com";
 const corsOptions = {
     origin: [CLIENT_URL],
     credentials: true,
